5225a5ce9076d3942dde2ea03c60314f4272a15e
Punnamanikumar <punnamanikumar@gmail.com>
2022-02-15 15:11:00 +0530

    ReactDay4

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import FormHandling from "./day4/FormHandling";
+import Parent from "./day4/Props/Parent";
+function App() {
+  return (
+    <div className="App">
+      <FormHandling />
+      <br />
+      <Parent />
+    </div>
+  );
+}
+
+export default App;
